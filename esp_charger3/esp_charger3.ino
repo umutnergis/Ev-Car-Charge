8a70e09a697f23199def1b08ff7eebf72e3d7497
umut nergis <94764998+umutnergis@users.noreply.github.com>
2024-08-14 14:36:49 +0300

    Baudrate and some current read optimized in accharge.ino

--- esp_charger3/esp_charger3.ino
+++ esp_charger3/esp_charger3.ino
@@ -5,10 +5,10 @@
 #include "esp_system.h"
 
 //#define DEBUG
-#define check10A 0x20 
-#define check16A 0x21 
-#define check24A 0x22 
-#define check32A 0x23
+#define check10A 0x22
+#define check16A 0x23
+#define check24A 0x24
+#define check32A 0x25
 
 #define RXp2 16
 #define TXp2 17
@@ -24,7 +24,7 @@ DNSServer dnsServer;
 WebServer server(80);
 
 String receivedData = "";
-int kwh = 0;
+float kwh = 0;
 float temp;
 int voltage1 = 0, voltage2 = 0, voltage3 = 0;
 double current1 = 0, current2 = 0, current3 = 0;
@@ -36,12 +36,12 @@ unsigned long previousMillis = 0;
 String charge_sit = "NO";
 String amper_sit = "0 A";
 
-void thread1(void* pvParameters);
-void thread2(void* pvParameters);
+void thread1(void *pvParameters);
+void thread2(void *pvParameters);
 
 void setup() {
   Serial.begin(115200);
-  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
+  Serial2.begin(57600, SERIAL_8N1, RXp2, TXp2);
   delay(500);
 
   xTaskCreatePinnedToCore(thread1, "Task1", 10000, NULL, 1, &Task1, 0);
@@ -51,13 +51,12 @@ void setup() {
 }
 
 void loop() {
-
 }
 
-void thread1(void* pvParameters) {
-  for(;;) {
-    
-    if(Serial2.available()) {
+void thread1(void *pvParameters) {
+  for (;;) {
+
+    if (Serial2.available()) {
       char c = Serial2.read();
       receivedData += c;
       if (c == '\n') {
@@ -66,10 +65,16 @@ void thread1(void* pvParameters) {
       }
     }
 
-    kwh = ((230*current1*0.9)+(230*current2*0.9)+(230*current3*0.9)); 
-    
-    if(Serial.available()) {
+    kwh = (((230 * current1 * 0.9) + (230 * current2 * 0.9) + (230 * current3 * 0.9))/1000);
+  
+    if (Serial.available()) {
       byte incomingByte = Serial.read();
+
+      char hexValue[3];
+      sprintf(hexValue, "%02X", incomingByte);
+      #ifdef DEBUG
+      Serial.println(hexValue);
+      #endif
       check_button(incomingByte);
     }
     sendFunction();
@@ -77,14 +82,12 @@ void thread1(void* pvParameters) {
   }
 }
 
-void thread2(void* pvParameters) {
-  for(;;) {
+void thread2(void *pvParameters) {
+  for (;;) {
     dnsServer.processNextRequest();
     server.handleClient();
     vTaskDelay(pdMS_TO_TICKS(50));
-    
   }
-
 }
 
 void acces_point() {
@@ -101,16 +104,14 @@ void acces_point() {
     }
   }
 
-  
   WiFi.mode(WIFI_AP);
-  WiFi.setTxPower(WIFI_POWER_19_5dBm);
+  WiFi.setTxPower(WIFI_POWER_20_5dBm);
 
-  
   WiFi.softAP(ssid, password, bestChannel);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  
+
   Serial.println("Access Point Başlatıldı");
-  
+
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP adresi: ");
   Serial.println(IP);
@@ -213,8 +214,8 @@ void handleStop() {
 void sendNumberToVP(byte vp1, byte vp2, int number) {
   byte num_high_byte = (number >> 8) & 0xFF;
   byte num_low_byte = number & 0xFF;
-  
-  byte packet[] = {0x5A, 0xA5, 0x06, 0x82, vp1, vp2, num_high_byte, num_low_byte};
+
+  byte packet[] = { 0x5A, 0xA5, 0x06, 0x82, vp1, vp2, num_high_byte, num_low_byte };
 
   for (byte i = 0; i < sizeof(packet); i++) {
     Serial.write(packet[i]);
@@ -226,42 +227,48 @@ void sendFunction() {
   if (currentMillis - previousMillis >= 300) {
     previousMillis = currentMillis;
     sendNumberToVP(0x50, 0x00, kwh);
-    temp = temperatureRead();  
-    #ifdef DEBUG
+    temp = temperatureRead();
+#ifdef DEBUG
     Serial.println("Data sent to VP");
-    #endif
+#endif
   }
 }
 
+/* void switchToPage(uint16_t pageNumber)
+{
+  uint8_t command[] = {0x5A, 0xA5, 0x07, 0x82, 0x00, 0x84, 0x5A, 0x01, (uint8_t)(pageNumber >> 8), (uint8_t)pageNumber};
+  Serial.write(command, sizeof(command));
+} */
+
 void check_button(byte coming) {
-  switch(coming) {
+  switch (coming) {
     case check10A:
       Serial2.println("s0");
       amper_sit = "10 A";
-      #ifdef DEBUG
-      Serial.println("10A buton algılandı");
-      #endif
+#ifdef DEBUG
+      Serial.println("10A buton algılandı----------------------------------------------------------");
+#endif
       break;
     case check16A:
       Serial2.println("s1");
       amper_sit = "16 A";
-      #ifdef DEBUG
-      Serial.println("16A buton algılandı");
-      #endif
+#ifdef DEBUG
+      Serial.println("16A buton algılandı----------------------------------------------------------");
+#endif
       break;
     case check24A:
       Serial2.println("s2");
       amper_sit = "24 A";
-      #ifdef DEBUG
-      Serial.println("24A buton algılandı");
-      #endif
+#ifdef DEBUG
+      Serial.println("24A buton algılandı----------------------------------------------------------");
+#endif
       break;
     case check32A:
       Serial2.println("s3");
       amper_sit = "32 A";
-      #ifdef DEBUG
-      Serial.println("32A buton algılandı");
-      #endif
+#ifdef DEBUG
+      Serial.println("32A buton algılandı----------------------------------------------------------");
+#endif
       break;
   }
 }
@@ -269,52 +276,46 @@ void check_button(byte coming) {
 void parsedata(String data) {
   if (data.startsWith("v1")) {
     voltage1 = data.substring(2).toInt();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Voltage 1: ");
     Serial.println(voltage1);
-    #endif
-  } 
-  else if (data.startsWith("v2")) {
+#endif
+  } else if (data.startsWith("v2")) {
     voltage2 = data.substring(2).toInt();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Voltage 2: ");
     Serial.println(voltage2);
-    #endif
-  } 
-  else if (data.startsWith("v3")) {
+#endif
+  } else if (data.startsWith("v3")) {
     voltage3 = data.substring(2).toInt();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Voltage 3: ");
     Serial.println(voltage3);
-    #endif
-  } 
-  else if (data.startsWith("c1")) {
+#endif
+  } else if (data.startsWith("c1")) {
     current1 = data.substring(2).toFloat();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Current 1: ");
     Serial.println(current1);
-    #endif
-  } 
-  else if (data.startsWith("c2")) {
+#endif
+  } else if (data.startsWith("c2")) {
     current2 = data.substring(2).toFloat();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Current 2: ");
     Serial.println(current2);
-    #endif
-  } 
-  else if (data.startsWith("c3")) {
+#endif
+  } else if (data.startsWith("c3")) {
     current3 = data.substring(2).toFloat();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Current 3: ");
     Serial.println(current3);
-    #endif
-  }
-  else if (data.startsWith("te")) {
+#endif
+  } else if (data.startsWith("te")) {
     temperature = data.substring(2).toInt();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Temperature: ");
     Serial.println(temperature);
-    #endif
+#endif
   }
   /* else if (data.startsWith("pv")) {
     coming_pvc = data.substring(2).toInt();
@@ -325,9 +326,9 @@ void parsedata(String data) {
   } */
   else if (data.startsWith("cr")) {
     coming_critical = data.substring(2).toInt();
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Critical number: ");
     Serial.println(coming_critical);
-    #endif
+#endif
   }
 }
